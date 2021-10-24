74e736a479e9bae7f77c5b2d4860a1cb14ddeea9
masumi <ju88991@gmail.com>
2021-10-24 15:15:28 +0900

    ok

--- game_theory.py
+++ game_theory.py
@@ -1,44 +1,96 @@
+import random
+import textwrap
+
 # 利得行列
-A = [[40, 30], [50, 20]]
-B = [[45, 50], [35, 25]]
+A = []
+B = []
+for i in range(2):
+    A.append([random.randrange(10, 60, 5), random.randrange(10, 60, 5)])
+    B.append([random.randrange(10, 60, 5), random.randrange(10, 60, 5)])
 
-def get_index(lists, value):
-    for list in lists:
-        if value in list:
-            return lists.index(list)
+test = False
+if test:
+    A = [[10, 30], [40, 15]]
+    B = [[20, 40], [30, 15]]
 
-answer_list = []
+X_X = (A[0][0],B[0][0])
+X_Y = (A[0][1],B[0][1])
+Y_X = (A[1][0],B[1][0])
+Y_Y = (A[1][1],B[1][1])
+string = textwrap.dedent('''
+[利得行列]
+--------------------------------
+                    B社
+              戦略X     戦略Y
+A社  戦略X  {X_X}   {X_Y}
+     戦略Y  {Y_X}   {Y_Y} 
+--------------------------------
+''').format(X_X=X_X, X_Y=X_Y, Y_X=Y_X,Y_Y=Y_Y)
+print(string)
 
-# B = xで固定したときのAの最大値
-a_score = max(A[0][0], A[1][0])
-a_index = get_index(A, a_score)
-answer = (a_score, B[a_index][0])
-answer_list.append(answer)
+b_answer = []
+# B = xで固定
+# Aの最大値
+score = max(A[0][0], A[1][0])
+skip = False
+# インデックス番号
+if score == A[0][0] and score == A[1][0]:
+    skip = True
+if not skip and score == A[0][0]:
+    idx = 0
+elif not skip and score == A[1][0]:
+    idx = 1
+if not skip:
+    b_answer.append((score, B[idx][0]))
 
 # B = yで固定したときのAの最大値
 # Aの最大値
-a_score = max(A[0][1], A[1][1])
-a_index = get_index(A, a_score)
-answer = (a_score, B[a_index][1])
-answer_list.append(answer)
+score = max(A[0][1], A[1][1])
+skip = False
+if score == A[0][1] and score == A[1][1]:
+    skip = True
+if not skip and score == A[0][1]:
+    idx = 0
+elif not skip and score == A[1][1]:
+    idx = 1
+if not skip:
+    b_answer.append((score, B[idx][1]))
 
-
+a_answer = []
 # A = xで固定したときのBの最大値
-score = max(B[0])
-# Bのインデックス番号を取得
-index = B[0].index(score)
-# 解
-answer = (A[0][index], score)
-if answer not in answer_list:
-    answer_list.append(answer)
+score = max(B[0][0], B[0][1])
+skip = False
+if score == B[0][0] and score == B[0][1]:
+    skip == True
+if not skip and score == B[0][0]:
+    idx = 0
+elif not skip and score == B[0][1]:
+    idx = 1
+if not skip:
+    a_answer.append((A[0][idx], score))
 
 # A = yで固定したときのBの最大値
-score = max(B[1])
-# Bのインデックス番号を取得
-index = B[1].index(score)
-# 解
-answer = (A[1][index], score)
-if answer not in answer_list:
-    answer_list.append(answer)
+score = max(B[1][0], B[1][1])
+skip = False
+print(score, B[1][0],B[1][1])
+if score == B[1][0] and score == B[1][1]:
+    skip = True
+if not skip and score == B[1][0]:
+    idx = 0
+elif not skip and score == B[1][1]:
+    idx = 1
+if not skip:
+    a_answer.append((A[1][idx], score))
 
-print(answer_list)
+nash = []
+for a in a_answer:
+    if a in b_answer:
+        nash.append(a)
+
+if len(nash) == 0:
+    print("解無し")
+else:
+    print("[ナッシュ均衡解]")
+    print(nash)
+print("A ans:", a_answer)
+print("B ans:", b_answer)
